5c1bf8e36462d5be696e873548e3b9d105d1c601
Carlos Morales <cfmorales191@gmail.com>
2023-11-04 18:41:55 -0600

    FrmOtelo
    Se agrega el form del tablero y el inicio del juego No se ha conectado nada, tiene implementado El FrmOtelo que se maximice automaticamente

---
+++ src/otelo/View/View.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package otelo.View;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author UTN
+ */
+public interface View {
+    public void display ();
+    public void displayMsg(String msg);
+    public void displayMax(JFrame Form );
+}
--- src/otelo/logica/Tablero.java
+++ src/otelo/logica/Tablero.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package otelo;
+package otelo.logica;
 
-import otelo.Colores;
+import otelo.logica.Colores;
 /**
  *
  * @author UTN
